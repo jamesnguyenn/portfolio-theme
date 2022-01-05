bfb42287c33f51c5e475f763f015b3fcef8e563e
unknown <nguyenquangthien151095@gmail.com>
2022-01-05 21:30:32 +0700

    Push Portfolio Theme

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Selector
+
+$ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
+
+//Listener
+
+//Selector
+
+$ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
+
+let navLink = $$(".nav-link");
+console.log(navLink);
+
+//Listener
+navLink.addEventListener("click", () => {
+  navLink.forEach((item) => {
+    item.class;
+  });
+});
+//Function
